6c39aaae87f4b599ddcfb5bc374b705c2593874b
Maxim Tkachenko <tkachenkonino1996@gmail.com>
2020-01-30 19:47:10 +0300

    генерируем пины на карту

--- js/main.js
+++ js/main.js
@@ -1,68 +1,58 @@
 'use strict';
 var numberOfAdd = 8;
-var arr = [];
 var addArr = [];
-var getRandom = function (n) {
-  return Math.round(Math.random() * n)
-};
-// Cоздает массив случайной уникальной последоватльности 8 элементов
-var createArr = function (n) {
-  var i = 1;
-  arr[0] = getRandom(n-1) + 1;
-  while (arr.length < n) {
-    var a = getRandom(n);
-    if (arr.indexOf(a) === -1 && a !== 0) {
-      arr[i] = a;
-      i++;
-    }
-  }
-};
-// создает массив данных имеющихся пользователей
-var buildAdd = function (arr) {
-arr.push({
-  author: {
-    avatar: 'img/avatars/iser0' + 1 + '.png'
-  },
-  offer: {
-    title: 'Царские хоромы',
-    address: '600, 350',
-    price: '400 т.р.',
-    type: 'palace',
-    rooms: 4,
-    guests: 2,
-    checkin: '12:00',
-    checkout: '13:00',
-    features: 'push arr',
-    description: '',
-    photos: 'arr photo'
-  },
-  location: {
-    x: getRandom(400),
-    y: getRandom(630)
-  }
-})
+var titles = ['Уютное гнездышко для молодоженов', 'Маленькая квартирка рядом с парком', 'Небольшая лавочка в парке', 'Императорский дворец в центре Токио'];
+var getRandom = function (min, max) {
+  return Math.round(min + (Math.random() * (max - 1)));
 };
 
-for ( var j = 0; j < numberOfAdd; j++ ) {
+// создает массив данных имеющихся пользователей
+var buildAdd = function (array) {
+  array.push({
+    author: {
+      avatar: 'img/avatars/user0' + getRandom(1, 8) + '.png'
+    },
+    offer: {
+      title: titles[getRandom(0, 3)],
+      address: '600, 350',
+      price: '400 т.р.',
+      type: 'palace',
+      rooms: 4,
+      guests: 2,
+      checkin: '12:00',
+      checkout: '13:00',
+      features: 'push arr',
+      description: '',
+      photos: 'arr photo'
+    },
+    location: {
+      x: getRandom(0, 1150),
+      y: getRandom(130, 630)
+    }
+  });
+};
+
+for (var j = 0; j < numberOfAdd; j++) {
   buildAdd(addArr);
 }
-console.log(addArr);
 
 document.querySelector('.map').classList.remove('map--faded');
 
 var similarMap = document.querySelector('.map__pins');
 var similarAddPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-var renderAddPin = function (arr) {
+var renderAddPin = function (creatArr) {
   var AddPin = similarAddPinTemplate.cloneNode(true);
-  AddPin.querySelector('map__pin').style = 'left: ' + arr.location.x + 'px; top: ' + arr.location.y + 'px;';
-  AddPin.src = arr.author.avatar;
-  AddPin.alt = 'Продам гараж';
+  AddPin.style = 'left:' + creatArr.location.x + 'px; top:' + creatArr.location.y + 'px;';
+  AddPin.querySelector('img').setAttribute('src', creatArr.author.avatar);
+  AddPin.querySelector('img').setAttribute('alt', creatArr.offer.title);
   return AddPin;
 };
 
 var fragment = document.createDocumentFragment();
-for (var q = 0; q < addArr.length; q++) {
-  fragment.appendChild(renderAddPin(addArr));
-}
+
+addArr.forEach(function (currentItem) {
+  fragment.appendChild(renderAddPin(currentItem));
+
+});
 
 similarMap.appendChild(fragment);
